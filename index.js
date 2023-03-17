e41b9ee1acdb59b2ec5252e8d901492b8be00a47
Tom Nielsen <tanielsen@gmail.com>
2023-03-17 12:04:23 +0000

    comments, rearrange

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//imports
 const {
   ul,
   li,
@@ -20,181 +21,13 @@ const {
 const renderLayout = require("@saltcorn/markup/layout");
 const db = require("@saltcorn/data/db");
 
-const subItem = (currentUrl) => (item) =>
-  li(
-    { class: "nav-item" },
-    item.link
-      ? a(
-          {
-            class: ["nav-link", active(currentUrl, item) && "active"],
-            href: text(item.link),
-          },
-          item.icon
-            ? i({ class: `nav-icon ${item.icon}` })
-            : i({ class: "far fa-circle nav-icon" }),
-          p(item.label)
-        )
-      : a(
-          {
-            class: ["nav-link"],
-            href: "javascript:;",
-          },
-          item.label
-        )
-  );
-
-const active = (currentUrl, item) =>
-  (item.link && currentUrl.startsWith(item.link)) ||
-  (item.subitems &&
-    item.subitems.some((si) => si.link && currentUrl.startsWith(si.link)));
-
-const sideBarItem = (currentUrl) => (item) => {
-  const is_active = active(currentUrl, item);
-  return li(
-    {
-      class: ["nav-item", item.subitems && is_active && "menu-open"],
-    },
-    item.link
-      ? a(
-          { class: ["nav-link", is_active && "active"], href: text(item.link) },
-          item.icon ? i({ class: `nav-icon ${item.icon}` }) : "",
-
-          p(text(item.label))
-        )
-      : item.subitems
-      ? [
-          a(
-            {
-              class: ["nav-link", is_active && "active"],
-              href: "javascript:;",
-            },
-            item.icon ? i({ class: `nav-icon ${item.icon}` }) : "",
-            p(text(item.label), i({ class: "end fas fa-angle-left" }))
-          ),
-          ul(
-            {
-              class: ["nav nav-treeview"],
-            },
-            item.subitems.map(subItem(currentUrl))
-          ),
-        ]
-      : span({ class: "nav-link" }, text(item.label))
-  );
-};
-
-const sideBarSection = (currentUrl) => (section) =>
-  [
-    section.section &&
-      section.section !== "Menu" &&
-      li({ class: "nav-header" }, section.section),
-    section.items.map(sideBarItem(currentUrl)).join(""),
-  ];
-
-const sidebar = (brand, sections, currentUrl) =>
-  aside(
-    { class: "main-sidebar sidebar-bg-dark sidebar-color-primary shadow" },
-    div(
-      { class: "brand-container" },
-      a(
-        {
-          class: "brand-link",
-          href: "/",
-        },
-        brand.logo &&
-          img({
-            src: brand.logo,
-            width: "30",
-            height: "30",
-            class: "brand-image opacity-75 shadow",
-            alt: "Logo",
-            loading: "lazy",
-          }),
-        span({ class: "brand-text font-weight-light" }, brand.name)
-      ),
-      a(
-        {
-          class: "pushmenu mx-1",
-          "data-lte-toggle": "sidebar-mini",
-          href: "javascript:;",
-          role: "button",
-        },
-        i({ class: "fas fa-angle-double-left" })
-      )
-    ),
-    div(
-      { class: "sidebar" },
-      nav(
-        { class: "mt-2" },
-        ul(
-          {
-            class: "nav nav-pills nav-sidebar flex-column",
-            "data-lte-toggle": "treeview",
-            role: "menu",
-            "data-accordion": "false",
-            id: "accordionSidebar",
-          },
-          sections.map(sideBarSection(currentUrl))
-        )
-      )
-    )
-  );
-
-const blockDispatch = {
-  pageHeader: ({ title, blurb }) =>
-    div(
-      h1({ class: "h3 mb-0 mt-2 text-gray-800" }, title),
-      blurb && p({ class: "mb-0 text-gray-800" }, blurb)
-    ),
-  footer: ({ contents }) =>
-    div(
-      { class: "container" },
-      footer(
-        { id: "footer" },
-        div({ class: "row" }, div({ class: "col-sm-12" }, contents))
-      )
-    ),
-  hero: ({ caption, blurb }) =>
-    header(
-      { class: "masthead" },
-      div(
-        { class: "container h-100" },
-        div(
-          {
-            class:
-              "row h-100 align-items-center justify-content-center text-center",
-          },
-          div(
-            { class: "col-lg-10 align-self-end" },
-            h1({ class: "text-uppercase font-weight-bold" }, caption),
-            hr({ class: "divider my-4" })
-          ),
-          div(
-            { class: "col-lg-8 align-self-baseline" },
-            p({ class: "font-weight-light mb-5" }, blurb)
-            /*a(
-              {
-                class: "btn btn-primary btn-xl",
-                href: "#about"
-              },
-              "Find Out More"
-            )*/
-          )
-        )
-      )
-    ),
-};
-const renderBody = (title, body) =>
-  renderLayout({
-    blockDispatch,
-    layout:
-      typeof body === "string" ? { type: "card", title, contents: body } : body,
-  });
-
-//const safeSlash = () => (isNode ? "/" : "");
+// http url prefix for serving static assets from public/
 const servePrefix = `/plugins/public/adminlte@${
   require("./package.json").version
 }`;
 
+// This is the core function for Saltcorn layout. It takes as argument an object with
+// the specification for the page and must return a string with the full rendered page.
 const wrap = ({
   title,
   menu,
@@ -261,6 +94,183 @@ const wrap = ({
   </body>
 </html>`;
 
+// render the body by calling back to Saltcorn's renderLayout, supplying the
+// custom blockDispatch, which specifies how certain layout elements should be rendered
+const renderBody = (title, body) =>
+  renderLayout({
+    blockDispatch,
+    layout:
+      typeof body === "string" ? { type: "card", title, contents: body } : body,
+  });
+
+const blockDispatch = {
+  pageHeader: ({ title, blurb }) =>
+    div(
+      h1({ class: "h3 mb-0 mt-2 text-gray-800" }, title),
+      blurb && p({ class: "mb-0 text-gray-800" }, blurb)
+    ),
+  footer: ({ contents }) =>
+    div(
+      { class: "container" },
+      footer(
+        { id: "footer" },
+        div({ class: "row" }, div({ class: "col-sm-12" }, contents))
+      )
+    ),
+  hero: ({ caption, blurb }) =>
+    header(
+      { class: "masthead" },
+      div(
+        { class: "container h-100" },
+        div(
+          {
+            class:
+              "row h-100 align-items-center justify-content-center text-center",
+          },
+          div(
+            { class: "col-lg-10 align-self-end" },
+            h1({ class: "text-uppercase font-weight-bold" }, caption),
+            hr({ class: "divider my-4" })
+          ),
+          div(
+            { class: "col-lg-8 align-self-baseline" },
+            p({ class: "font-weight-light mb-5" }, blurb)
+            /*a(
+              {
+                class: "btn btn-primary btn-xl",
+                href: "#about"
+              },
+              "Find Out More"
+            )*/
+          )
+        )
+      )
+    ),
+};
+
+//render the sidebar with menu and brand. Note yhis is not exported and is not part of any
+// official API, these functions are just called by `wrap`
+const sidebar = (brand, sections, currentUrl) =>
+  aside(
+    { class: "main-sidebar sidebar-bg-dark sidebar-color-primary shadow" },
+    div(
+      { class: "brand-container" },
+      a(
+        {
+          class: "brand-link",
+          href: "/",
+        },
+        brand.logo &&
+          img({
+            src: brand.logo,
+            width: "30",
+            height: "30",
+            class: "brand-image opacity-75 shadow",
+            alt: "Logo",
+            loading: "lazy",
+          }),
+        span({ class: "brand-text font-weight-light" }, brand.name)
+      ),
+      a(
+        {
+          class: "pushmenu mx-1",
+          "data-lte-toggle": "sidebar-mini",
+          href: "javascript:;",
+          role: "button",
+        },
+        i({ class: "fas fa-angle-double-left" })
+      )
+    ),
+    div(
+      { class: "sidebar" },
+      nav(
+        { class: "mt-2" },
+        ul(
+          {
+            class: "nav nav-pills nav-sidebar flex-column",
+            "data-lte-toggle": "treeview",
+            role: "menu",
+            "data-accordion": "false",
+            id: "accordionSidebar",
+          },
+          sections.map(sideBarSection(currentUrl))
+        )
+      )
+    )
+  );
+
+const sideBarSection = (currentUrl) => (section) =>
+  [
+    section.section &&
+      section.section !== "Menu" &&
+      li({ class: "nav-header" }, section.section),
+    section.items.map(sideBarItem(currentUrl)).join(""),
+  ];
+
+const sideBarItem = (currentUrl) => (item) => {
+  const is_active = active(currentUrl, item);
+  return li(
+    {
+      class: ["nav-item", item.subitems && is_active && "menu-open"],
+    },
+    item.link
+      ? a(
+          { class: ["nav-link", is_active && "active"], href: text(item.link) },
+          item.icon ? i({ class: `nav-icon ${item.icon}` }) : "",
+
+          p(text(item.label))
+        )
+      : item.subitems
+      ? [
+          a(
+            {
+              class: ["nav-link", is_active && "active"],
+              href: "javascript:;",
+            },
+            item.icon ? i({ class: `nav-icon ${item.icon}` }) : "",
+            p(text(item.label), i({ class: "end fas fa-angle-left" }))
+          ),
+          ul(
+            {
+              class: ["nav nav-treeview"],
+            },
+            item.subitems.map(subItem(currentUrl))
+          ),
+        ]
+      : span({ class: "nav-link" }, text(item.label))
+  );
+};
+
+const subItem = (currentUrl) => (item) =>
+  li(
+    { class: "nav-item" },
+    item.link
+      ? a(
+          {
+            class: ["nav-link", active(currentUrl, item) && "active"],
+            href: text(item.link),
+          },
+          item.icon
+            ? i({ class: `nav-icon ${item.icon}` })
+            : i({ class: "far fa-circle nav-icon" }),
+          p(item.label)
+        )
+      : a(
+          {
+            class: ["nav-link"],
+            href: "javascript:;",
+          },
+          item.label
+        )
+  );
+
+// Helper function to figure out if a menu item is active.
+const active = (currentUrl, item) =>
+  (item.link && currentUrl.startsWith(item.link)) ||
+  (item.subitems &&
+    item.subitems.some((si) => si.link && currentUrl.startsWith(si.link)));
+
+// render an alert
 const alert = (type, s) => {
   //console.log("alert", type, s,s.length)
   const realtype = type === "error" ? "danger" : type;
